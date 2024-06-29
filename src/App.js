e2c4a86bc2bfcd23f33244dcd5b6107dd13cc626
Your GitHub Username <davilama@unlv.nevada.edu>
2024-06-29 12:00:59 -0700

    classwork

--- src/App.js
+++ src/App.js
@@ -1,46 +1,58 @@
-import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
-import './App.css';
+import "./App.css";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Card from 'react-bootstrap/Card';
 
-import Home from './components/Home'
-import About from './components/About'
-import Packages from './components/Packages'
-
+import Home from "./components/Home";
+import About from "./components/About";
+import Packages from "./components/Packages";
 
 function App() {
-  
-  const packages = ['Activate your Crystals', 'Monkey Meditation', 'Soak in the Hotsprings', 'Hypnotherapy', 'Mineral Bath']
-
+  const packages = [
+    "Activate your Crystals",
+    "Monkey Meditation",
+    "Soak in the Hotsprings",
+    "Hypnotherapy",
+    "Mineral Bath",
+  ];
 
   return (
     <div className="App">
       <Router>
         <header>
-          <h1 className="title">Welcome to Monty's Mineral SPA</h1>
+          <h1 className="title"></h1>
 
           <div className="navBar">
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/about">About Us</Link>
-              </li>
-              <li>
-                <Link to="/packages">Our Packages</Link>
-              </li>
-            </ul>
           </div>
+          <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">Welcome to Monty's Mineral SPA</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Link to="/">Home</Link>
+            <Link to="/about">About Us</Link>
+            <Link to="/packages">Our Packages</Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
         </header>
 
         <div className="display">
-          <Route path="/" component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/packages" render={() => <Packages packages={packages}/>}  />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route
+              path="/packages"
+              element={<Packages packages={packages} />}
+            />
+          </Routes>
         </div>
-
       </Router>
-
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,16 @@
 
+import Card from 'react-bootstrap/Card';
 
 export default function Home () {
     return(
-        <div className="homepage">
+        <Card className="homepage">
             <h1>Visit our resort and change your life!</h1>
 
             <h1>Grand opening in StressVille, Texas</h1>
             
             <h1>Check out our wide array of packages and offers!</h1>
 
-        </div>
+        </Card>
     )
 
 }
